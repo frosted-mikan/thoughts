995f04603ffdf897b184811c836b64ff98f03b75
ahuo <amberhuo2012@gmail.com>
2021-05-20 15:22:03 -0400

    calendar open source

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hello world it's ambo</Text>
+    // <View style={styles.container}>
+    <View>
+      <CalendarList
+        // Callback which gets executed when visible months change in scroll view. Default = undefined
+        onVisibleMonthsChange={(months) => {console.log('now these months are visible', months);}}
+        // Max amount of months allowed to scroll to the past. Default = 50
+        pastScrollRange={50}
+        // Max amount of months allowed to scroll to the future. Default = 50
+        futureScrollRange={50}
+        // Enable or disable scrolling of calendar list
+        scrollEnabled={true}
+        // Enable or disable vertical scroll indicator. Default = false
+        showScrollIndicator={true}
+        // Callback that gets called on day press
+        onDayPress={(day)=>{
+          console.log('day pressed', day);
+        }}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
